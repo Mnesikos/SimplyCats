95962fb37468deebc2aae57bfb625ff5638fa5d9
Mnesikos <mnesikos@gmail.com>
2020-01-03 12:58:41 -0500

    finalizing bowl and litter box
    - bowls now accept water
    - bowl blockstate json now uses forge
    - both blocks save their states when world reloads

--- 1.12.2/src/main/java/com/github/mnesikos/simplycats/block/BlockBowl.java
+++ 1.12.2/src/main/java/com/github/mnesikos/simplycats/block/BlockBowl.java
@@ -6,16 +6,25 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionUtils;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.ChunkCache;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -28,7 +37,7 @@ public class BlockBowl extends BlockTileEntity<TileEntityBowl> {
     public static final int GUI_ID = 0;
 
     public static final PropertyBool FULL_FOOD = PropertyBool.create("full_food");
-    public static final PropertyBool FULL_WATER = PropertyBool.create("full_water");
+    public static final PropertyInteger WATER_LEVEL = PropertyInteger.create("water_level", 0, 3);
 
     public BlockBowl(String name) {
         super(Material.GROUND, name);
@@ -37,14 +46,79 @@ public class BlockBowl extends BlockTileEntity<TileEntityBowl> {
         this.setHardness(0.2F);
         this.setDefaultState(this.blockState.getBaseState()
                 .withProperty(FULL_FOOD, Boolean.FALSE)
-                .withProperty(FULL_WATER, Boolean.FALSE));
+                .withProperty(WATER_LEVEL, Integer.valueOf(0)));
+    }
+
+    public void setWaterLevel(World world, BlockPos pos, IBlockState state, int level) {
+        world.setBlockState(pos, state.withProperty(WATER_LEVEL, Integer.valueOf(MathHelper.clamp(level, 0, 3))), 2);
+        world.updateComparatorOutputLevel(pos, this);
     }
 
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
-        if (!world.isRemote)
-            player.openGui(SimplyCats.instance, GUI_ID, world, pos.getX(), pos.getY(), pos.getZ());
-        return true;
+        TileEntityBowl tileEntityBowl = this.getTileEntity(world, pos);
+
+        if (tileEntityBowl == null)
+            return false;
+        else {
+            int waterLevel = ((Integer)state.getValue(WATER_LEVEL)).intValue();
+            ItemStack itemstack = player.getHeldItem(hand);
+
+            if (tileEntityBowl.isEmpty() && itemstack.getItem() == Items.WATER_BUCKET && waterLevel < 3 && !world.isRemote) {
+                if (!player.capabilities.isCreativeMode)
+                    player.setHeldItem(hand, new ItemStack(Items.BUCKET));
+
+                this.setWaterLevel(world, pos, state, 3);
+                world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                return true;
+
+            } else if (tileEntityBowl.isEmpty() && itemstack.getItem() == Items.BUCKET && waterLevel == 3 && !world.isRemote) {
+                if (!player.capabilities.isCreativeMode) {
+                    itemstack.shrink(1);
+
+                    if (itemstack.isEmpty())
+                        player.setHeldItem(hand, new ItemStack(Items.WATER_BUCKET));
+                    else if (!player.inventory.addItemStackToInventory(new ItemStack(Items.WATER_BUCKET)))
+                        player.dropItem(new ItemStack(Items.WATER_BUCKET), false);
+                }
+
+                this.setWaterLevel(world, pos, state, 0);
+                world.playSound((EntityPlayer)null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                return true;
+
+            } else if (tileEntityBowl.isEmpty() && itemstack.getItem() == Items.POTIONITEM && PotionUtils.getPotionFromItem(itemstack) == PotionTypes.WATER && waterLevel < 3 && !world.isRemote) {
+                if (!player.capabilities.isCreativeMode) {
+                    player.setHeldItem(hand, new ItemStack(Items.GLASS_BOTTLE));
+                    if (player instanceof EntityPlayerMP)
+                        ((EntityPlayerMP)player).sendContainerToPlayer(player.inventoryContainer);
+                }
+
+                this.setWaterLevel(world, pos, state, waterLevel + 1);
+                world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                return true;
+
+            } else if (waterLevel == 0 && !world.isRemote)
+                player.openGui(SimplyCats.instance, GUI_ID, world, pos.getX(), pos.getY(), pos.getZ());
+
+            return true;
+        }
+    }
+
+    @Override
+    public void fillWithRain(World world, BlockPos pos) {
+        TileEntityBowl tileEntityBowl = this.getTileEntity(world, pos);
+        if (tileEntityBowl.isEmpty()) {
+            if (world.rand.nextInt(20) == 1) {
+                float f = world.getBiome(pos).getTemperature(pos);
+
+                if (world.getBiomeProvider().getTemperatureAtHeight(f, pos.getY()) >= 0.15F) {
+                    IBlockState iblockstate = world.getBlockState(pos);
+
+                    if (((Integer) iblockstate.getValue(WATER_LEVEL)).intValue() < 3)
+                        world.setBlockState(pos, iblockstate.cycleProperty(WATER_LEVEL), 2);
+                }
+            }
+        }
     }
 
     @Override
@@ -54,6 +128,14 @@ public class BlockBowl extends BlockTileEntity<TileEntityBowl> {
             InventoryHelper.dropInventoryItems(world, pos, (IInventory)te);
 
         super.breakBlock(world, pos, state);
+    }
+
+    public boolean hasComparatorInputOverride(IBlockState state) {
+        return true;
+    }
+
+    public int getComparatorInputOverride(IBlockState blockState, World worldIn, BlockPos pos) {
+        return ((Integer)blockState.getValue(WATER_LEVEL)).intValue();
     }
 
     @Override
@@ -73,25 +155,25 @@ public class BlockBowl extends BlockTileEntity<TileEntityBowl> {
         if (tileEntity instanceof TileEntityBowl) {
             TileEntityBowl tileEntityBowl = (TileEntityBowl) tileEntity;
             return getDefaultState().withProperty(FULL_FOOD, !tileEntityBowl.isEmpty())
-                    /*.withProperty(FULL_WATER, )*/;
+                    .withProperty(WATER_LEVEL, state.getValue(WATER_LEVEL));
         }
         return state;
     }
 
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState();
+        return this.getDefaultState().withProperty(WATER_LEVEL, Integer.valueOf(meta));
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
-        return 0;
+        return ((Integer)state.getValue(WATER_LEVEL)).intValue();
     }
 
     @Override
     protected BlockStateContainer createBlockState() {
         return new BlockStateContainer(this, new IProperty[] {
-                FULL_FOOD, FULL_WATER
+                FULL_FOOD, WATER_LEVEL
         });
     }
 
--- 1.12.2/src/main/java/com/github/mnesikos/simplycats/block/BlockLitterBox.java
+++ 1.12.2/src/main/java/com/github/mnesikos/simplycats/block/BlockLitterBox.java
@@ -6,6 +6,14 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -35,19 +43,82 @@ public class BlockLitterBox extends BlockBase {
         world.setBlockState(pos, state.withProperty(DIRTY, isDirty), 2);
     }
 
-    /*@Override
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        ItemStack itemstack = playerIn.getHeldItem(hand);
+        boolean isThisFull = worldIn.getBlockState(pos).getValue(FULL);
+        boolean isThisDirty = worldIn.getBlockState(pos).getValue(DIRTY);
+
+        if (!isThisFull) {
+            if (itemstack.getItem() != Item.getItemFromBlock(Blocks.SAND))
+                return false;
+
+            setFull(worldIn, pos, state, true);
+            if (!playerIn.capabilities.isCreativeMode)
+                itemstack.shrink(1);
+
+        } else if (!isThisDirty) {
+            if (itemstack.getItem() == Items.BONE) // todo remove testing feature
+                setDirty(worldIn, pos, state, true);
+            else {
+                ItemStack returnSand = new ItemStack(Blocks.SAND);
+                if (!playerIn.capabilities.isCreativeMode) {
+                    if (itemstack.isEmpty())
+                        playerIn.setHeldItem(hand, returnSand);
+                    else if (!playerIn.addItemStackToInventory(returnSand))
+                        playerIn.dropItem(returnSand, false);
+                }
+
+                setFull(worldIn, pos, state, false);
+            }
+
+        } else {
+            if (itemstack.getItem() != Item.getItemFromBlock(Blocks.SAND))
+                return false;
+
+            setDirty(worldIn, pos, state, false);
+            if (!playerIn.capabilities.isCreativeMode)
+                itemstack.shrink(1);
+        }
+
+        worldIn.notifyBlockUpdate(pos, state, state, 3);
+        return true;
+    }
+
+    @Override
+    public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        super.getDrops(drops, world, pos, state, fortune);
+        if (state.getValue(FULL) && !state.getValue(DIRTY))
+            drops.add(new ItemStack(Blocks.SAND));
+    }
+
+    @Override
     public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        return getDefaultState().withProperty(FULL, )
-                .withProperty(DIRTY, );
-    }*/
+        return getDefaultState().withProperty(FULL, state.getValue(FULL))
+                .withProperty(DIRTY, state.getValue(DIRTY));
+        //return state;
+    }
 
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState();
+        switch (meta) {
+            case 0:
+            default:
+                return this.getDefaultState();
+            case 1:
+                return this.getDefaultState().withProperty(FULL, true).withProperty(DIRTY, false);
+            case 2:
+                return this.getDefaultState().withProperty(FULL, true).withProperty(DIRTY, true);
+        }
+        //return this.getDefaultState();
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
+        if (state.getValue(FULL) && !state.getValue(DIRTY))
+            return 1;
+        if (state.getValue(FULL) && state.getValue(DIRTY))
+            return 2;
         return 0;
     }
 
