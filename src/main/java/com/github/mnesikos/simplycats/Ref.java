4e349de3e08c849ee3f725580cb3cb5f7c6b2998
Tiffany Jager <tjager@caltech.edu>
2021-05-04 19:37:11 -0400

    Refactor to avoid the use of proxies

--- src/main/java/com/github/mnesikos/simplycats/Ref.java
+++ src/main/java/com/github/mnesikos/simplycats/Ref.java
@@ -20,8 +20,6 @@ public class Ref {
     public static final String VERSION = "@VERSION@";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
-    public static final String CLIENT_PROXY = "com.github.mnesikos.simplycats.proxy.ClientProxy";
-    public static final String SERVER_PROXY = "com.github.mnesikos.simplycats.proxy.CommonProxy";
     private static final Map<UUID, String> CUSTOM_CATS = Maps.newHashMap();
 
     private static List<Item> EDIBLE = new ArrayList<>();
--- src/main/java/com/github/mnesikos/simplycats/item/CatItems.java
+++ src/main/java/com/github/mnesikos/simplycats/item/CatItems.java
@@ -2,14 +2,25 @@ package com.github.mnesikos.simplycats.item;
 
 import com.github.mnesikos.simplycats.Ref;
 import com.github.mnesikos.simplycats.SimplyCats;
-import com.github.mnesikos.simplycats.item.*;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CatItems {
+    public static final CreativeTabs CREATIVE_TAB = new CreativeTabs(Ref.MODID + ".tab") {
+        @Override
+        public ItemStack getTabIconItem() {
+            return new ItemStack(PET_CARRIER);
+        }
+    };
+
+    // A list of metadata variants that the client will register models for
+    public static final List<ItemVariant> VARIANTS = new ArrayList<>();
+
     public static final List<Item> ITEMS = new ArrayList<>();
     public static final ItemCatBook CAT_BOOK = registerItem("cat_book", new ItemCatBook());
     public static final ItemPetCarrier PET_CARRIER = registerItem("pet_carrier", new ItemPetCarrier());
@@ -27,8 +38,36 @@ public class CatItems {
     public static <T extends Item> T registerItem(String name, T item) {
         item.setRegistryName(name);
         item.setUnlocalizedName(Ref.MODID + "." + name);
-        item.setCreativeTab(SimplyCats.PROXY.SIMPLYCATS);
+        item.setCreativeTab(CREATIVE_TAB);
         ITEMS.add(item);
         return item;
+    }
+
+    // Adds the variants for each item to the list of variants
+    public static void collectModelVariants() {
+        for (Item item : CatItems.ITEMS) {
+            VARIANTS.add(new ItemVariant(item, 0, item.getRegistryName().getResourcePath()));
+        }
+        // Skip 0 because that's already added
+        for (int i = 1; i < PET_CARRIER.getNumModels(); i++) {
+            VARIANTS.add(new ItemVariant(PET_CARRIER, i, "pet_carrier"));
+        }
+        // These may be better as separate items
+        VARIANTS.add(new ItemVariant(CERTIFICATE, 0, "certificate_adopt"));
+        VARIANTS.add(new ItemVariant(CERTIFICATE, 1, "certificate_release"));
+    }
+
+    // Used by the client for registering item models
+    // If Java had structs, this would be a struct
+    public static class ItemVariant {
+        public final int meta;
+        public final String name;
+        public final Item item;
+
+        public ItemVariant(Item item, int meta, String name) {
+            this.meta = meta;
+            this.name = name;
+            this.item = item;
+        }
     }
 }
