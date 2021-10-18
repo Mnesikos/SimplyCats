3afddd65006fcf03eefbe72545f64e796f48112c
Mnesikos <mnesikos@gmail.com>
2021-10-18 19:33:49 -0400

    add timers to render with name tags... maybe lol

--- src/main/java/com/github/mnesikos/simplycats/client/render/entity/SimplyCatRenderer.java
+++ src/main/java/com/github/mnesikos/simplycats/client/render/entity/SimplyCatRenderer.java
@@ -4,16 +4,21 @@ import com.github.mnesikos.simplycats.SCReference;
 import com.github.mnesikos.simplycats.SimplyCats;
 import com.github.mnesikos.simplycats.client.model.entity.SimplyCatModel;
 import com.github.mnesikos.simplycats.entity.SimplyCatEntity;
+import com.github.mnesikos.simplycats.entity.core.Genetics;
 import com.google.common.collect.Maps;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ForgeHooksClient;
 
 import java.util.Map;
 
@@ -64,5 +69,33 @@ public class SimplyCatRenderer extends MobRenderer<SimplyCatEntity, SimplyCatMod
     @Override
     protected void renderNameTag(SimplyCatEntity cat, ITextComponent textComponent, MatrixStack matrixStack, IRenderTypeBuffer renderTypeBuffer, int p_225629_5_) { // todo
         super.renderNameTag(cat, textComponent, matrixStack, renderTypeBuffer, p_225629_5_);
+
+        double distance = this.entityRenderDispatcher.distanceToSqr(cat);
+        if (ForgeHooksClient.isNameplateInRenderDistance(cat, distance) && this.entityRenderDispatcher.camera.getEntity().isShiftKeyDown() && !cat.isFixed() && !cat.isBaby()) {
+            boolean catNotSneaking = !cat.isDiscrete();
+            float height = cat.getBbHeight() + 0.62F;
+
+            matrixStack.pushPose();
+            matrixStack.translate(0.0D, height, 0.0D);
+            matrixStack.mulPose(this.entityRenderDispatcher.cameraOrientation());
+            matrixStack.scale(-0.012F, -0.012F, 0.012F);
+            Matrix4f matrix4f = matrixStack.last().pose();
+
+            float backgroundOpacity = Minecraft.getInstance().options.getBackgroundOpacity(0.25F);
+            int j = (int) (backgroundOpacity * 255.0F) << 24;
+
+            TranslationTextComponent info = new TranslationTextComponent((cat.getSex() == Genetics.Sex.FEMALE ? (cat.getBreedingStatus("inheat") ? "chat.info.in_heat" : "chat.info.not_in_heat") : "chat.info.male"), cat.getMateTimer());
+            if (cat.getBreedingStatus("ispregnant"))
+                info = new TranslationTextComponent(cat.getBreedingStatus("inheat") ? "chat.info.pregnant_heat" : "chat.info.pregnant", cat.getMateTimer());
+
+            FontRenderer fontRenderer = this.getFont();
+            float centeredPos = (float) (-fontRenderer.width(info) / 2);
+
+            fontRenderer.drawInBatch(info, centeredPos, 0, 553648127, false, matrix4f, renderTypeBuffer, catNotSneaking, j, p_225629_5_);
+            if (catNotSneaking)
+                fontRenderer.drawInBatch(info, centeredPos, 0, -1, false, matrix4f, renderTypeBuffer, false, 0, p_225629_5_);
+
+            matrixStack.popPose();
+        }
     }
 }
