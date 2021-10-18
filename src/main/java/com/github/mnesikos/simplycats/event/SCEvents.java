6e3c60d8bcc413947f1602d3a9695bc8bb77fb53
Mnesikos <mnesikos@gmail.com>
2021-10-18 18:45:29 -0400

    Update SCEvents.java

--- src/main/java/com/github/mnesikos/simplycats/event/SCEvents.java
+++ src/main/java/com/github/mnesikos/simplycats/event/SCEvents.java
@@ -1,6 +1,5 @@
 package com.github.mnesikos.simplycats.event;
 
-import com.github.mnesikos.simplycats.configuration.SCConfig;
 import com.github.mnesikos.simplycats.entity.SimplyCatEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -8,48 +7,23 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.AvoidEntityGoal;
 import net.minecraft.entity.monster.CreeperEntity;
 import net.minecraft.entity.monster.WitchEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.Util;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.ForgeRegistries;
-
-import javax.annotation.Nullable;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
 
 public class SCEvents {
-    private static Set<Class<? extends EntityType>> entities;
-
-    public static boolean isEntityPrey(@Nullable Entity entity) {
-        if (entities == null) {
-            entities = new HashSet<>();
-            for (String s : SCConfig.Common.prey_list.get()) {
-                ResourceLocation location = new ResourceLocation(s);
-                if (ForgeRegistries.ENTITIES.containsKey(location))
-                    entities.add(Objects.requireNonNull(ForgeRegistries.ENTITIES.getValue(location)).getClass());
-            }
-        }
-        return entities.contains(entity.getType());
-    }
-
     public static boolean isRatEntity(Entity entity) {
         String entityType = EntityType.getKey(entity.getType()).toString();
         return entityType.equals("rats:rat")/* || entityType.equals("zawa:brownrat")*/;
     }
 
-    @SubscribeEvent
-    public void onPlayerLogsIn(PlayerEvent.PlayerLoggedInEvent event) {
+    /*@SubscribeEvent
+    public void onPlayerLogsIn(PlayerEvent.PlayerLoggedInEvent event) { // todo
         if (SCConfig.Common.join_message.get()) {
             PlayerEntity player = event.getPlayer();
             player.sendMessage(new TranslationTextComponent("chat.join.cat_count", player.getPersistentData().getInt("CatCount")), Util.NIL_UUID);
         }
-    }
+    }*/
 
     @SubscribeEvent
     public void onEntityJoinWorldEvent(EntityJoinWorldEvent event) {
@@ -61,13 +35,11 @@ public class SCEvents {
 
     @SubscribeEvent
     public void onLivingSetAttackTargetEvent(LivingSetAttackTargetEvent event) {
-        if (event.getTarget() != null && event.getEntityLiving() instanceof LivingEntity) {
-            LivingEntity attackingEntity = (LivingEntity) event.getEntityLiving();
-            if (attackingEntity instanceof WitchEntity) {
-                if (!attackingEntity.level.getEntitiesOfClass(SimplyCatEntity.class, attackingEntity.getBoundingBox().inflate(16.0F)).isEmpty()) {
+        if (event.getTarget() != null && event.getEntityLiving() != null) {
+            LivingEntity attackingEntity = event.getEntityLiving();
+            if (attackingEntity instanceof WitchEntity)
+                if (!attackingEntity.level.getEntitiesOfClass(SimplyCatEntity.class, attackingEntity.getBoundingBox().inflate(16.0F)).isEmpty())
                     ((WitchEntity) attackingEntity).setTarget(null);
-                }
-            }
         }
     }
 }
