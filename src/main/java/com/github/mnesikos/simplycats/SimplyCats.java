de0bd16a424f817eb9c79b4364c6efb75f5502b3
Mnesikos <mnesikos@gmail.com>
2024-03-18 13:52:10 -0400

    add cat bowls pt. 1

--- src/main/java/com/github/mnesikos/simplycats/SimplyCats.java
+++ src/main/java/com/github/mnesikos/simplycats/SimplyCats.java
@@ -1,6 +1,7 @@
 package com.github.mnesikos.simplycats;
 
 import com.github.mnesikos.simplycats.block.SCBlocks;
+import com.github.mnesikos.simplycats.client.color.ColorEvents;
 import com.github.mnesikos.simplycats.configuration.SCConfig;
 import com.github.mnesikos.simplycats.entity.SimplyCatEntity;
 import com.github.mnesikos.simplycats.event.SCSounds;
@@ -11,12 +12,14 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -51,6 +54,10 @@ public class SimplyCats {
         SCVillagers.POI_TYPES.register(modBus);
 
         forgeBus.addListener(SCWorldGen::setupVillageWorldGen);
+
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            modBus.addListener(ColorEvents::registerColorHandlerBlocks);
+        }
 
         forgeBus.register(CatDataFixer.class);
     }
