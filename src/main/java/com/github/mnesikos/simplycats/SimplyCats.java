ae9f45f7577df81a0713cd65917f3b0834070eae
Mnesikos <mnesikos@gmail.com>
2025-06-13 19:48:23 -0400

    datagen recipes

--- src/main/java/com/github/mnesikos/simplycats/SimplyCats.java
+++ src/main/java/com/github/mnesikos/simplycats/SimplyCats.java
@@ -3,12 +3,14 @@ package com.github.mnesikos.simplycats;
 import com.github.mnesikos.simplycats.block.SCBlocks;
 import com.github.mnesikos.simplycats.client.color.ColorEvents;
 import com.github.mnesikos.simplycats.configuration.SCConfig;
+import com.github.mnesikos.simplycats.data.SCRecipeProvider;
 import com.github.mnesikos.simplycats.entity.SimplyCatEntity;
 import com.github.mnesikos.simplycats.event.SCSounds;
 import com.github.mnesikos.simplycats.item.SCItems;
 import com.github.mnesikos.simplycats.worldgen.villages.SCVillagers;
 import com.github.mnesikos.simplycats.worldgen.villages.SCWorldGen;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
@@ -18,6 +20,7 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -50,25 +53,31 @@ public class SimplyCats {
             .build("cat"));
 
     public SimplyCats() {
-        IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, SCConfig.SPEC);
 
-        ENTITIES.register(modBus);
-        SCItems.REGISTRAR.register(modBus);
-        SCBlocks.REGISTRAR.register(modBus);
-        CREATIVE_MODE_TABS.register(modBus);
-        SCSounds.REGISTRAR.register(modBus);
-        SCVillagers.PROFESSIONS.register(modBus);
-        SCVillagers.POI_TYPES.register(modBus);
+        ENTITIES.register(bus);
+        SCItems.REGISTRAR.register(bus);
+        SCBlocks.REGISTRAR.register(bus);
+        CREATIVE_MODE_TABS.register(bus);
+        SCSounds.REGISTRAR.register(bus);
+        SCVillagers.PROFESSIONS.register(bus);
+        SCVillagers.POI_TYPES.register(bus);
 
-        forgeBus.addListener(SCWorldGen::setupVillageWorldGen);
+        bus.addListener(this::gatherData);
+
+        MinecraftForge.EVENT_BUS.addListener(SCWorldGen::setupVillageWorldGen);
 
         if (FMLEnvironment.dist == Dist.CLIENT) {
-            modBus.addListener(ColorEvents::registerColorHandlerBlocks);
+            bus.addListener(ColorEvents::registerColorHandlerBlocks);
         }
 
-        forgeBus.register(CatDataFixer.class);
+        MinecraftForge.EVENT_BUS.register(CatDataFixer.class);
+    }
+
+    private void gatherData(final GatherDataEvent event) {
+        DataGenerator dataGenerator = event.getGenerator();
+        dataGenerator.addProvider(event.includeServer(), new SCRecipeProvider(dataGenerator.getPackOutput()));
     }
 }
---
+++ src/main/java/com/github/mnesikos/simplycats/data/SCRecipeProvider.java
@@ -0,0 +1,209 @@
+package com.github.mnesikos.simplycats.data;
+
+import com.github.mnesikos.simplycats.block.SCBlocks;
+import com.github.mnesikos.simplycats.item.SCItems;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.*;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.properties.WoodType;
+
+import java.util.function.Consumer;
+
+public class SCRecipeProvider extends RecipeProvider {
+    public SCRecipeProvider(PackOutput packOutput) {
+        super(packOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCItems.CAT_BOOK.get())
+                .requires(Items.BOOK)
+                .requires(SCItems.CATNIP.get())
+                .unlockedBy("has_catnip", has(SCItems.CATNIP.get())).unlockedBy("has_book", has(Items.BOOK)).save(consumer);
+        oneToOneConversionRecipe(consumer, SCItems.CAT_BOOK.get(), SCItems.CAT_BOOK.get(), "");
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCBlocks.SHELTER_BOOK.get())
+                .requires(SCItems.CAT_BOOK.get())
+                .requires(SCItems.CATNIP.get())
+                .unlockedBy("has_cat_book", has(SCItems.CAT_BOOK.get())).save(consumer);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCItems.STERILIZE_POTION.get())
+                .requires(Items.GLASS_BOTTLE)
+                .requires(SCItems.CATNIP.get())
+                .requires(Items.SPIDER_EYE)
+                .unlockedBy("has_catnip", has(SCItems.CATNIP.get())).save(consumer);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCItems.TREAT_BAG.get())
+                .requires(Items.LEATHER)
+                .requires(Items.IRON_INGOT)
+                .unlockedBy("has_leather", has(Items.LEATHER)).save(consumer);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCItems.ADOPT_CERTIFICATE.get())
+                .requires(Items.PAPER)
+                .requires(Items.BLACK_DYE)
+                .unlockedBy("has_black_dye", has(Items.BLACK_DYE)).unlockedBy("has_paper", has(Items.PAPER)).save(consumer);
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, SCItems.RELEASE_CERTIFICATE.get())
+                .requires(SCItems.ADOPT_CERTIFICATE.get())
+                .unlockedBy("has_adopt_certificate", has(SCItems.ADOPT_CERTIFICATE.get())).save(consumer);
+        oneToOneConversionRecipe(consumer, SCItems.ADOPT_CERTIFICATE.get(), SCItems.RELEASE_CERTIFICATE.get(), "");
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, SCItems.LASER_POINTER.get())
+                .pattern(" B")
+                .pattern("RI")
+                .define('B', Items.STONE_BUTTON)
+                .define('R', Items.REDSTONE_TORCH)
+                .define('I', Items.IRON_INGOT)
+                .unlockedBy("has_redstone", has(Items.REDSTONE)).save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.NAME_TAG)
+                .pattern("S ")
+                .pattern(" I")
+                .define('S', Items.STRING)
+                .define('I', Ingredient.of(Items.IRON_INGOT, Items.GOLD_INGOT))
+                .unlockedBy("has_string", has(Items.STRING)).save(consumer);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, SCItems.PET_CARRIER.get())
+                .pattern("ISI")
+                .pattern("SBS")
+                .pattern("ISI")
+                .define('I', Items.IRON_INGOT)
+                .define('B', Items.IRON_BARS)
+                .define('S', Items.TERRACOTTA)
+                .unlockedBy("has_iron", has(Items.IRON_INGOT)).save(consumer);
+
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.WHITE).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.WHITE).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.WHITE).get(), Blocks.WHITE_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.ORANGE).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.ORANGE).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.ORANGE).get(), Blocks.ORANGE_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.MAGENTA).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.MAGENTA).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.MAGENTA).get(), Blocks.MAGENTA_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.LIGHT_BLUE).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.LIGHT_BLUE).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.LIGHT_BLUE).get(), Blocks.LIGHT_BLUE_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.YELLOW).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.YELLOW).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.YELLOW).get(), Blocks.YELLOW_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.LIME).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.LIME).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.LIME).get(), Blocks.LIME_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.PINK).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.PINK).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.PINK).get(), Blocks.PINK_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.GRAY).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.GRAY).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.GRAY).get(), Blocks.GRAY_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.LIGHT_GRAY).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.LIGHT_GRAY).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.LIGHT_GRAY).get(), Blocks.LIGHT_GRAY_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.CYAN).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.CYAN).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.CYAN).get(), Blocks.CYAN_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.PURPLE).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.PURPLE).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.PURPLE).get(), Blocks.PURPLE_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.BLUE).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.BLUE).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.BLUE).get(), Blocks.BLUE_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.BROWN).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.BROWN).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.BROWN).get(), Blocks.BROWN_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.GREEN).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.GREEN).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.GREEN).get(), Blocks.GREEN_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.RED).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.RED).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.RED).get(), Blocks.RED_CARPET);
+        catTreePieces(consumer, SCBlocks.CAT_TREE_BEDS.get(DyeColor.BLACK).get(), SCBlocks.CAT_TREE_BOXES.get(DyeColor.BLACK).get(), SCBlocks.CAT_TREE_POSTS.get(DyeColor.BLACK).get(), Blocks.BLACK_CARPET);
+
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.WHITE).get(), Blocks.WHITE_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.ORANGE).get(), Blocks.ORANGE_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.MAGENTA).get(), Blocks.MAGENTA_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.LIGHT_BLUE).get(), Blocks.LIGHT_BLUE_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.YELLOW).get(), Blocks.YELLOW_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.LIME).get(), Blocks.LIME_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.PINK).get(), Blocks.PINK_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.GRAY).get(), Blocks.GRAY_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.LIGHT_GRAY).get(), Blocks.LIGHT_GRAY_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.CYAN).get(), Blocks.CYAN_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.PURPLE).get(), Blocks.PURPLE_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.BLUE).get(), Blocks.BLUE_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.BROWN).get(), Blocks.BROWN_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.GREEN).get(), Blocks.GREEN_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.RED).get(), Blocks.RED_TERRACOTTA);
+        catBowl(consumer, SCBlocks.CAT_BOWLS.get(DyeColor.BLACK).get(), Blocks.BLACK_TERRACOTTA);
+
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.WHITE).get(), Blocks.WHITE_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.ORANGE).get(), Blocks.ORANGE_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.MAGENTA).get(), Blocks.MAGENTA_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.LIGHT_BLUE).get(), Blocks.LIGHT_BLUE_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.YELLOW).get(), Blocks.YELLOW_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.LIME).get(), Blocks.LIME_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.PINK).get(), Blocks.PINK_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.GRAY).get(), Blocks.GRAY_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.LIGHT_GRAY).get(), Blocks.LIGHT_GRAY_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.CYAN).get(), Blocks.CYAN_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.PURPLE).get(), Blocks.PURPLE_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.BLUE).get(), Blocks.BLUE_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.BROWN).get(), Blocks.BROWN_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.GREEN).get(), Blocks.GREEN_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.RED).get(), Blocks.RED_TERRACOTTA);
+        litterBox(consumer, SCBlocks.LITTER_BOXES.get(DyeColor.BLACK).get(), Blocks.BLACK_TERRACOTTA);
+
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.OAK.name()).get(), Blocks.OAK_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.SPRUCE.name()).get(), Blocks.SPRUCE_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.BIRCH.name()).get(), Blocks.BIRCH_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.ACACIA.name()).get(), Blocks.ACACIA_PLANKS);
+//        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.CHERRY.name()).get(), Blocks.CHERRY_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.JUNGLE.name()).get(), Blocks.JUNGLE_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.DARK_OAK.name()).get(), Blocks.DARK_OAK_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.CRIMSON.name()).get(), Blocks.CRIMSON_PLANKS);
+        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.WARPED.name()).get(), Blocks.WARPED_PLANKS);
+//        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.MANGROVE.name()).get(), Blocks.MANGROVE_PLANKS);
+//        scratchingPost(consumer, SCBlocks.SCRATCHING_POSTS.get(WoodType.BAMBOO.name()).get(), Blocks.BAMBOO_PLANKS);
+
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.OAK.name()).get(), Blocks.OAK_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.SPRUCE.name()).get(), Blocks.SPRUCE_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.BIRCH.name()).get(), Blocks.BIRCH_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.ACACIA.name()).get(), Blocks.ACACIA_PLANKS);
+//        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.CHERRY.name()).get(), Blocks.CHERRY_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.JUNGLE.name()).get(), Blocks.JUNGLE_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.DARK_OAK.name()).get(), Blocks.DARK_OAK_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.CRIMSON.name()).get(), Blocks.CRIMSON_PLANKS);
+        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.WARPED.name()).get(), Blocks.WARPED_PLANKS);
+//        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.MANGROVE.name()).get(), Blocks.MANGROVE_PLANKS);
+//        windowPerch(consumer, SCBlocks.WINDOW_PERCHES.get(WoodType.BAMBOO.name()).get(), Blocks.BAMBOO_PLANKS);
+    }
+
+    protected static void catBowl(Consumer<FinishedRecipe> consumer, ItemLike bowl, ItemLike terracotta) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, bowl)
+                .pattern("C C")
+                .pattern(" C ")
+                .define('C', terracotta)
+                .unlockedBy(getHasName(terracotta), has(terracotta)).save(consumer);
+    }
+
+    protected static void litterBox(Consumer<FinishedRecipe> consumer, ItemLike litterBox, ItemLike terracotta) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, litterBox)
+                .pattern("CCC")
+                .pattern("C C")
+                .pattern("CCC")
+                .define('C', terracotta)
+                .unlockedBy(getHasName(terracotta), has(terracotta)).save(consumer);
+    }
+
+    protected static void scratchingPost(Consumer<FinishedRecipe> consumer, ItemLike scratchingPost, ItemLike planks) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, scratchingPost)
+                .pattern(" P ")
+                .pattern("STS")
+                .pattern("PPP")
+                .define('P', planks)
+                .define('S', Items.STRING)
+                .define('T', Items.STICK)
+                .unlockedBy(getHasName(planks), has(planks)).save(consumer);
+    }
+
+    protected static void windowPerch(Consumer<FinishedRecipe> consumer, ItemLike windowPerch, ItemLike planks) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, windowPerch, 4)
+                .pattern("PPP")
+                .pattern("T T")
+                .define('P', planks)
+                .define('T', Items.STICK)
+                .unlockedBy(getHasName(planks), has(planks)).save(consumer);
+    }
+
+    protected static void catTreePieces(Consumer<FinishedRecipe> consumer, ItemLike bed, ItemLike box, ItemLike post, ItemLike carpet) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, bed, 4)
+                .pattern("C C")
+                .pattern("CCC")
+                .define('C', carpet)
+                .unlockedBy(getHasName(carpet), has(carpet)).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, box, 4)
+                .pattern("CCC")
+                .pattern("C C")
+                .pattern("CCC")
+                .define('C', carpet)
+                .unlockedBy(getHasName(carpet), has(carpet)).save(consumer);
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, post, 4)
+                .pattern(" C ")
+                .pattern("SWS")
+                .pattern(" C ")
+                .define('C', carpet)
+                .define('S', Items.STRING)
+                .define('W', Items.STICK)
+                .unlockedBy(getHasName(carpet), has(carpet)).save(consumer);
+    }
+}
